179735adc6be55dc9854a093dd358898c22d404d
denis <okposhka09@gmail.com>
2021-11-30 14:21:51 +0700

    Add project files.

---
+++ CV19/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace CV19
+{
+    public partial class App : Application
+    {
+        public static bool IsDesignMode { get; private set; } = true;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            IsDesignMode = false;
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using CV19.Infrastructure.Commands;
+using CV19.Models.Decanat;
+using CV19.ViewModels.Base;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+        public ObservableCollection<Group> Groups { get; }
+
+        public object[] CompositeCollection { get; }
+
+        #region SelectedCompositeValue : object - Выбранный непонятный элемент
+
+        /// <summary>Выбранный непонятный элемент</summary>
+        private object _SelectedCompositeValue;
+
+        /// <summary>Выбранный непонятный элемент</summary>
+        public object SelectedCompositeValue { get => _SelectedCompositeValue; set => Set(ref _SelectedCompositeValue, value); }
+
+        #endregion
+
+        #region SelectedGroup : Group - Выбранная группа
+
+        /// <summary>Выбранная группа</summary>
+        private Group _SelectedGroup;
+
+        /// <summary>Выбранная группа</summary>
+        public Group SelectedGroup
+        {
+            get => _SelectedGroup;
+            set
+            {
+                if (!Set(ref _SelectedGroup, value)) return;
+
+                _SelectedGroupStudents.Source = value?.Students;
+                OnPropertyChanged(nameof(SelectedGroupStudents));
+            }
+        }
+
+        #endregion
+
+        #region StudentFilterText : string - Текст фильтра студентов
+
+        /// <summary>Текст фильтра студентов</summary>
+        private string _StudentFilterText;
+
+        /// <summary>Текст фильтра студентов</summary>
+        public string StudentFilterText
+        {
+            get => _StudentFilterText;
+            set
+            {
+                if (!Set(ref _StudentFilterText, value)) return;
+                _SelectedGroupStudents.View.Refresh();
+            }
+        }
+
+        #endregion
+
+        #region SelectedGroupStudents
+
+        private readonly CollectionViewSource _SelectedGroupStudents = new CollectionViewSource();
+
+        private void OnStudentFiltred(object Sender, FilterEventArgs E)
+        {
+            if (!(E.Item is Student student))
+            {
+                E.Accepted = false;
+                return;
+            }
+
+            var filter_text = _StudentFilterText;
+            if (string.IsNullOrWhiteSpace(filter_text))
+                return;
+
+            if (student.Name is null || student.Surname is null || student.Patronymic is null)
+            {
+                E.Accepted = false;
+                return;
+            }
+
+            if(student.Name.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            if(student.Surname.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            if(student.Patronymic.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+
+            E.Accepted = false;
+        }
+
+        public ICollectionView SelectedGroupStudents => _SelectedGroupStudents?.View;
+
+        #endregion
+
+        #region SelectedPageIndex : int - Номер выбранной вкладки
+
+        /// <summary>Номер выбранной вкладки</summary>
+        private int _SelectedPageIndex = 1;
+
+        /// <summary>Номер выбранной вкладки</summary>
+        public int SelectedPageIndex
+        {
+            get => _SelectedPageIndex;
+            set => Set(ref _SelectedPageIndex, value);
+        }
+
+        #endregion
+
+        #region Заголовок окна
+
+        private string _Title = "Анализ статистики CV19";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+        #region Status : string - Статус программы
+
+        /// <summary>Статус программы</summary>
+        private string _Status = "Готов!";
+
+        /// <summary>Статус программы</summary>
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+
+        #endregion
+
+        public IEnumerable<Student> TestStudents =>
+            Enumerable.Range(1, App.IsDesignMode ? 10 : 100_000)
+               .Select(i => new Student
+               {
+                   Name = $"Имя {i}",
+                   Surname = $"Фамилия {i}"
+               });
+
+        public DirectoryViewModel DiskRootDir { get; } = new DirectoryViewModel("c:\\");
+
+        #region SelectedDirectory : DirectoryViewModel - Выбранная директория
+
+        /// <summary>Выбранная директория</summary>
+        private DirectoryViewModel _SelectedDirectory;
+
+        /// <summary>Выбранная директория</summary>
+        public DirectoryViewModel SelectedDirectory { get => _SelectedDirectory; set => Set(ref _SelectedDirectory, value); }
+
+        #endregion
+
+
+        #region Команды
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
+        #region ChangeTabIndexCommand
+
+        public ICommand ChangeTabIndexCommand { get; }
+
+        private bool CanChangeTabIndexCommandExecute(object p) => _SelectedPageIndex >= 0;
+
+        private void OnChangeTabIndexCommandExecuted(object p)
+        {
+            if (p is null) return;
+            SelectedPageIndex += Convert.ToInt32(p);
+        }
+
+        #endregion
+
+        public ICommand CreateGroupCommand { get; }
+
+        private bool CanCreateGroupCommandExecute(object p) => true;
+
+        private void OnCreateGroupCommandExecuted(object p)
+        {
+            var group_max_index = Groups.Count + 1;
+            var new_group = new Group
+            {
+                Name = $"Группа {group_max_index}",
+                Students = new ObservableCollection<Student>()
+            };
+
+            Groups.Add(new_group);
+        }
+
+        #region DeleteGroupCommand
+
+        public ICommand DeleteGroupCommand { get; }
+
+        private bool CanDeleteGroupCommandExecuted(object p) => p is Group group && Groups.Contains(group);
+
+        private void OnDeleteGroupCommandExecuted(object p)
+        {
+            if (!(p is Group group)) return;
+            var group_index = Groups.IndexOf(group);
+            Groups.Remove(group);
+            if (group_index < Groups.Count)
+                SelectedGroup = Groups[group_index];
+        }
+
+        #endregion
+
+        #endregion
+
+
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            ChangeTabIndexCommand = new LambdaCommand(OnChangeTabIndexCommandExecuted, CanChangeTabIndexCommandExecute);
+            CreateGroupCommand = new LambdaCommand(OnCreateGroupCommandExecuted, CanCreateGroupCommandExecute);
+            DeleteGroupCommand = new LambdaCommand(OnDeleteGroupCommandExecuted, CanDeleteGroupCommandExecuted);
+
+            #endregion
+
+            var student_index = 1;
+            var students = Enumerable.Range(1, 10).Select(i => new Student
+            {
+                Name = $"Name {student_index}",
+                Surname = $"Surname {student_index}",
+                Patronymic = $"Patronymic {student_index++}",
+                Birthday = DateTime.Now,
+                Rating = 0
+            });
+
+            var groups = Enumerable.Range(1, 20).Select(i => new Group
+            {
+                Name = $"Группа {i}",
+                Students = new ObservableCollection<Student>(students)
+            });
+
+            Groups = new ObservableCollection<Group>(groups);
+
+            var data_list = new List<object>();
+
+            data_list.Add("Helo World!");
+            data_list.Add(42);
+            var group = Groups[1];
+            data_list.Add(group);
+            data_list.Add(group.Students[0]);
+
+            CompositeCollection = data_list.ToArray();
+
+            _SelectedGroupStudents.Filter += OnStudentFiltred;
+        }
+
+
+
+    }
+}
